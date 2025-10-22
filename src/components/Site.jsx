6edcb1266306b3329ff3b3b0f0701d98b804c217
joon6390 <joon6390@naver.com>
2025-10-23 00:18:12 +0900

    사이트 영역 완료

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React from "react";
+
+const siteText = [
+    {
+        text: ["make", "site compliant with", "vite"],
+        title: "비트를 이용한 사이트 제작",
+        code: "https://github.com/joon6390/port-vite",
+        view: "https://port-vite.netlify.app",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : HTML5/CSS3, CSS Variable, Vite",
+        ],
+    },
+    {
+        text: ["make", "site compliant with", "react.js"],
+        title: "리액트를 이용한 사이트 제작",
+        code: "https://github.com/joon6390/port-react",
+        view: "https://port-react.netlify.app",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : HTML5/CSS3, CSS Variable, Vite",
+        ],
+    },
+    {
+        text: ["make", "site compliant with", "vue.js"],
+        title: "뷰를 이용한 사이트 제작",
+        code: "https://github.com/joon6390/port-vue",
+        view: "https://port-vue.netlify.app",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : HTML5/CSS3, Scss Variable, vue",
+        ],
+    },
+    {
+        text: ["make", "site compliant with", "next.js"],
+        title: "넥스트를 이용한 사이트 제작",
+        code: "https://github.com/joon6390/port-next",
+        view: "https://port-next.netlify.app",
+        info: [
+            "site coding",
+            "production period : two days",
+            "use stack : HTML5/CSS3, Scss Variable, next.js",
+        ],
+    },
+];
 
 const Site = () => {
-  return (
-    <div id='site'>Site</div>
-  )
+    return (
+        <section id="site">
+            <div className="site__inner">
+                <h2 className="site__title">
+                    Site coding <em>나의 작업물</em>
+                </h2>
+                <div className="site__wrap">
+                    {siteText.map((site, key) => (
+                        <article className={`site__item s${key+1}`} key={key}>
+                            <span className="num">{key+1}.</span>
+                            <div className="text">
+                                <div>{site.text[0]}</div>
+                                <div>{site.text[1]}</div>
+                                <div>{site.text[2]}</div>
+                            </div>
+                            <h3 className="title">
+                                {site.title}
+                            </h3>
+                            <div className="btn">
+                                <a href={site.code}>code</a>
+                                <a href={site.view}>view</a>
+                            </div>
+                            <div className="info">
+                                <span>{site.info[0]}</span>
+                                <span>{site.info[1]}</span>
+                                <span>{site.info[2]}</span>
+                            </div>
+                        </article>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
 }
 
-export default Site
+export default Site;
